8066f4a2e92645a68e9db6c754a3c5752feca135
Kyle Cesare <kcesare@gmail.com>
2011-12-26 18:09:14 -0700

    Initial binary serialization for the skyscraper simulation.

--- Seve/ILC.Seve.Examples.Skyscraper/SkyscraperBinarySerializer.cs
+++ Seve/ILC.Seve.Examples.Skyscraper/SkyscraperBinarySerializer.cs
@@ -1,18 +1,56 @@
-﻿using System;
+﻿using System.IO;
 using ILC.Seve.Genetics;
+using ILC.Seve.Graph;
 
 namespace ILC.Seve.Examples.Skyscraper
 {
     public class SkyscraperBinarySerializer : IBinarySerializer
     {
-        public byte[] ToBinary(Individual individual)
+        public byte[] ToBinary(Individual _individual)
         {
-            throw new NotImplementedException();
+            var individual = (SkyscraperIndividual)_individual;
+            var graph = individual.Graph;
+
+            var stream = new MemoryStream();
+
+            using (stream)
+            {
+                var writer = new BinaryWriter(stream);
+
+                // Write the length of the graph (int)
+                writer.Write(graph.Vertices.Count);
+
+                // Write the locations of each point on the graph (3 floats)
+                foreach (var vertex in graph.Vertices)
+                {
+                    writer.Write(vertex.X);
+                    writer.Write(vertex.Y);
+                    writer.Write(vertex.Z);
+                }
+            }
+
+            return stream.ToArray();
         }
 
         public Individual FromBinary(byte[] data)
         {
-            throw new NotImplementedException();
+            var graph = new VertexGraph();
+            var reader = new BinaryReader(new MemoryStream(data));
+
+            var count = reader.ReadInt32();
+
+            for (int i = 0; i < count; i++)
+            {
+                var vertex = new Vertex(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle());
+                graph.Vertices.Add(vertex);
+            }
+
+            var individual = new SkyscraperIndividual(graph);
+
+            // Use ConnectNearest to restore connections.  This is less than ideal.
+            individual.ConnectNearest(2);
+
+            return individual;
         }
     }
 }
