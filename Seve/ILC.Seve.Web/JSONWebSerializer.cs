6cec47f748fd7883df8a2a85ee686a152f01518e
Kyle Cesare <kcesare@gmail.com>
2012-02-21 18:15:55 -0800

    Add initial groundwork for web serialization.

---
+++ Seve/ILC.Seve.Web/JSONWebSerializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using ILC.Seve.Genetics;
+
+namespace ILC.Seve.Web
+{
+    public class JSONWebSerializer : WebSerializer
+    {
+        public string Serialize(List<Individual> individuals)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Seve/ILC.Seve.Web/WebSerializer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using ILC.Seve.Genetics;
+
+namespace ILC.Seve.Web
+{
+    public interface WebSerializer
+    {
+        string Serialize(List<Individual> individuals);
+    }
+}
