1768289700cffc1a4716a4bcc25c8b9dd5f99583
Kyle Cesare <kcesare@gmail.com>
2012-02-22 22:44:00 -0800

    Add documention for JSONWebSerializer.

--- Seve/ILC.Seve.Web/JSONWebSerializer.cs
+++ Seve/ILC.Seve.Web/JSONWebSerializer.cs
@@ -3,6 +3,17 @@ using ILC.Seve.Genetics;
 
 namespace ILC.Seve.Web
 {
+    /// <summary>
+    /// A serializer that transforms individuals into JSON strings.  Each string
+    /// is just a list of all the points.
+    /// </summary>
+    /// <example>
+    /// The following string represents an individual with 3 points (1, 2, 3),
+    /// (4, 5, 6), and (7, 8, 9).
+    /// <code>
+    /// [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    /// </code>
+    /// </example>
     public class JSONWebSerializer : WebSerializer
     {
         public string Serialize(Individual individual)
