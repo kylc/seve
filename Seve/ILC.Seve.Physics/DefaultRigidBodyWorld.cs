4853ee4625babc8d8186b6e3b23c3b64ba743c28
Kyle Cesare <kcesare@gmail.com>
2012-02-23 19:40:14 -0800

    Very initial support for rigid body simulations.  This doesn't work yet.

---
+++ Seve/ILC.Seve.Physics/DefaultRigidBodyWorld.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BulletSharp;
+using ILC.Seve.Graph;
+using ILC.Seve.Util;
+
+namespace ILC.Seve.Physics
+{
+    public class DefaultRigidBodyWorld : PhysicsWorld
+    {
+        private static DbvtBroadphase Broadphase;
+        private static DefaultCollisionConfiguration CollisionConfiguration;
+        private static CollisionDispatcher Dispatcher;
+        private static SequentialImpulseConstraintSolver Solver;
+        private static DiscreteDynamicsWorld DynamicsWorld;
+        private static RigidBody Ground;
+
+        private List<VertexBoundRigidBody> Individual;
+        private VertexGraph LastState;
+
+        static DefaultRigidBodyWorld()
+        {
+            // Broadphase algorithms are responsible for calculating bounding
+            // boxes.  We should probably use an AABB Tree (DbvtBroadphase)
+            // because they are generally good for worlds with lots of motion.
+            // Sweep and Prune Broadphases are best when most of the world is
+            // static.
+            Broadphase = new DbvtBroadphase();
+            CollisionConfiguration = new DefaultCollisionConfiguration();
+            Dispatcher = new CollisionDispatcher(CollisionConfiguration);
+            Solver = new SequentialImpulseConstraintSolver();
+
+            DynamicsWorld = new DiscreteDynamicsWorld(Dispatcher, Broadphase, Solver, CollisionConfiguration);
+            // Bullet uses a right-handed coordinate system, so Y is up, X is "right", and z is "left"
+            DynamicsWorld.Gravity = new Vector3(0F, -9.81F, 0F); // Y is the traditional Z axis
+
+            Ground = PhysicsHelpers.MakePlane(new Vector3(0, 1, 0), 0);
+            DynamicsWorld.AddRigidBody(Ground);
+        }
+        
+        public DefaultRigidBodyWorld(VertexGraph initialState)
+        {
+            AddVertexGraph(initialState);
+        }
+
+        private void AddVertexGraph(VertexGraph graph)
+        {
+            Individual = new List<VertexBoundRigidBody>();
+
+            foreach (var vertex in graph.Vertices)
+            {
+                var mass = 10000;
+                var motionState = new DefaultMotionState();
+                var collisionShape = new BoxShape(1);
+
+                var info = new RigidBodyConstructionInfo(mass, motionState, collisionShape);
+                var rigidBody = new VertexBoundRigidBody(vertex, info);
+
+                Individual.Add(rigidBody);
+            }
+
+            foreach (var body in Individual)
+            {
+                // Select the 3 nearest vertices, excluding this one
+                var nearest = Individual.OrderBy(a => a.Binding.DistanceTo(body.Binding))
+                                         .Where(a => a != body)
+                                         .Take(3);
+                foreach (var other in nearest)
+                {
+                    // TODO: What are these matrices supposed to be?
+                    var frameInA = Matrix.Identity;
+                    var frameInB = Matrix.Identity;
+
+                    // TODO: How do you specify the spring's springiness?
+                    var constraint = new Generic6DofSpringConstraint(body, other, frameInA, frameInB, true);
+
+                    // TODO: Now how do I apply this to the bodies?
+                }
+            }
+        }
+
+        public override void StepSimulation(int frequency)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override VertexGraph GetState()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Seve/ILC.Seve.Util/VertexBoundRigidBody.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BulletSharp;
+using ILC.Seve.Graph;
+
+namespace ILC.Seve.Util
+{
+    public class VertexBoundRigidBody : RigidBody
+    {
+        public Vertex Binding { get; set; }
+
+        public VertexBoundRigidBody(Vertex binding, RigidBodyConstructionInfo info)
+            : base(info)
+        {
+            this.Binding = binding;
+        }
+    }
+}
